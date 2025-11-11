7f4b39a259e7956a5fd9455e7a28eca40f19db6d
Spencer Walter <waltespe@shernet.sheridancollege.ca>
2025-11-11 12:57:48 -0500

    Implemented first version of acceleration and deceleration. The player accelerates to the left when pressing the A key, the player acceelerates to the left when pressing the D key. The player decelerates when not pressing one of those keys.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D selfRigidBody;
+    public float maxSpeed = 5;
+    public float accelerationTime = 2;
+    public float decelerationTime = 1;
+    public float acceleration;
+    public float deceleration;
+    public Vector2 velocity;
     public enum FacingDirection
     {
         left, right
@@ -12,7 +19,8 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        acceleration = maxSpeed / accelerationTime;
+        deceleration = maxSpeed / decelerationTime;
     }
 
     // Update is called once per frame
@@ -21,12 +29,32 @@ public class PlayerController : MonoBehaviour
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
         Vector2 playerInput = new Vector2();
-        MovementUpdate(playerInput);
+        if(Input.GetKey(KeyCode.A))
+        {
+            playerInput = Vector2.left;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            playerInput = Vector2.right;
+        }
+            MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
-
+        if (playerInput.magnitude > 0)
+        {
+            velocity += playerInput * acceleration * Time.deltaTime;
+            if (velocity.magnitude > maxSpeed)
+            {
+                velocity = velocity.normalized * maxSpeed;
+            }
+        }
+        else
+        {
+            velocity -= velocity.normalized * deceleration * Time.deltaTime;
+        }
+        selfRigidBody.linearVelocity = velocity;
     }
 
     public bool IsWalking()
